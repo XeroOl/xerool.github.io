f41a36c18a4965157d2c48cf5f2fe883c1c5872c
XeroOl <xerool@protonmail.com>
2024-12-29 15:04:19 -0600

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-if (window.location.pathname.startsWith('/notitg-mirin/')) {
+if (window.location.pathname.startsWith('/notitg-mirin')) {
     const newPath = window.location.pathname.replace('/notitg-mirin', '/mirin-template');
     const newUrl = `${window.location.origin}${newPath}`;
     window.location.replace(newUrl);
